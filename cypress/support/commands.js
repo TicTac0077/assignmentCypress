4e36d9e2efb09027173401e6c46ff6787888e23e
TicTac0077 <93879260+TicTac0077@users.noreply.github.com>
2025-01-17 17:50:11 +0500

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,13 @@ Cypress.Commands.add('highlight', { prevSubject: 'element' }, (subject) => {
     $el.css('border', '2px solid red');
   });
 });
+
+// Select a random value from the dropdown options
+Cypress.Commands.add("getIframeBody", (iframeSelector) => {
+  return cy
+    .get(iframeSelector)
+    .its("0.contentDocument.body") // Access the iframe's body
+    .should("not.be.empty") // Ensure the iframe is loaded
+    .then(cy.wrap); // Wrap the iframe body for further commands
+});
+
